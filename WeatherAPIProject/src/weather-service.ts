ee3f8a5ac5a69321f8bbfaac4b1d0d695e8abdf5
GrmBng <graeme.bing@gmail.com>
2022-10-16 19:15:59 +0200

    tim helped call api, how to impilment the api

--- WeatherAPIProject/src/weather-service.ts
+++ WeatherAPIProject/src/weather-service.ts
@@ -1,21 +1,10 @@
 // const GRAPHIC_URL =
 //   'http://www.7timer.info/bin/astro.php?lon=113.17&lat=23.09&ac=0&lang=en&unit=metric&output=internal&tzshift=0';
 
-const BASE_URL =
-  'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=xml';
+const BASE_URL = 'https://www.7timer.info/bin/civil.php';
 
-export function getLongitude() {
-  return fetch(`${BASE_URL}/lon`);
-}
-
-export function getLatitude() {
-  return fetch(`${BASE_URL}/lat`);
-}
-
-export function getProduct() {
-  return fetch(`${BASE_URL}/civil`);
-}
-
-export function getOutputStandard() {
-  return fetch(`${BASE_URL}/json`);
+export function getWeatherReport(latitude: number, longitude: number) {
+  return fetch(
+    `${BASE_URL}?lon=${longitude}&lat=${latitude}&ac=0&unit=metric&output=json&tzshift=2`
+  );
 }

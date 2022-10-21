dcd831656d699e7fa54e1381f94abb0c6a18e96d
GrmBng <graeme.bing@gmail.com>
2022-10-21 15:32:46 +0200

    bassic banner done

--- WeatherAPIProject/src/main.ts
+++ WeatherAPIProject/src/main.ts
@@ -1,4 +1,7 @@
 import { fetchUnwrapData } from './weatherVariables';
+import { bannerEditorAndExporter } from './weather-UI';
+import './style.css';
 //lon=28.2293&lat25.7479 pretoria cordinates
 // returnJson(28.2293, 25.7479);
+bannerEditorAndExporter();
 fetchUnwrapData(28.2293, 25.7479);
--- WeatherAPIProject/src/weather-UI.ts
+++ WeatherAPIProject/src/weather-UI.ts
@@ -1,1 +1,9 @@
 import './style.css';
+// learn how to us ts to populate dom
+export function bannerEditorAndExporter() {
+  const bannerElement = document.createElement('div');
+  bannerElement.className = 'weather-banner';
+  bannerElement.innerText = 'Choose your three day forcast.';
+  const body = document.getElementsByTagName('BODY')[0];
+  body.append(bannerElement);
+}
